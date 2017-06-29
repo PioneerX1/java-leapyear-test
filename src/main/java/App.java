65e6af227fff296864171a3f65abb92addcb49f9
Joel Bakken, Laura Hamilton and Mick Sexton <joel@>
2017-06-29 09:27:07 -0700

    add ui

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,14 @@
+import java.io.Console;
+
 public class App {
   public static void main(String[] args) {
-    //nothihg
+    while (true) {
+    Console myConsole = System.console();
+    System.out.println("Enter a year, we'll tell you if it's a leap year:");
+    int inputYear = Integer.parseInt(myConsole.readLine());
+    LeapYear leapYear = new LeapYear();
+    boolean leapYearResult = leapYear.isLeapYear(inputYear);
+    System.out.println("Is that a leap year? " + leapYearResult);
+  }
   }
 }
